25a633f0fb84314c3061e3e9db27bb34bbc5e628
yangjujamjam <yangjujamjam@gmail.com>
2025-03-07 10:01:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gasUrl = 'https://script.google.com/macros/s/AKfycbxXrjrYRyrN1sfwqFcEHaEtRKQJpNGvzAvuo9TgFxbLwIYU-XBGuWj9lahTQZ2GtCkU/exec'; // GAS 배포 후 URL 입력
+const gasUrl = 'https://script.google.com/macros/s/AKfycbxP3x6-OxrpYC8-78bIzHumTS9cEEbNpTBEdKzcjwR_drgOTuEiCNEHA4KyMqQWIUuQ/exec'; // GAS 배포 후 URL 입력
 
 function detectPlatform(text) {
   if (text.includes("야놀자")) return "야놀자";
