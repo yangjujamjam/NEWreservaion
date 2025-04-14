6941a02df5465b82754d863c617e3b31e6db7a26
yangjujamjam <yangjujamjam@gmail.com>
2025-04-14 12:40:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 /** =========================================
  *  [1] 전역 설정
  * ========================================= */
-const gasUrl = 'https://script.google.com/macros/s/AKfycbwI4NqfbFVMEaHkpWZPXmh-rAfitgcu3l4fldr9TIvRx-7fJtK_nm_IvVfjuW2gcXaG/exec';
+const gasUrl = 'https://script.google.com/macros/s/AKfycbyuVcVkFhW0AfmtTWmQUvips4PwmkOs-9tyU-Cm1aZBMEocq4qB64mcKpvLqrktjSic/exec';
 
 /** =========================================
  *  [2] 페이지 로드 시 초기 처리
